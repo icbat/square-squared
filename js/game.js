d9d1db7ca885d726e00857a08134a725661323eb
Evan Cobb <cobb.evan@gmail.com>
2016-02-25 22:59:33 -0600

    dude shows up

--- js/game.js
+++ js/game.js
@@ -4,11 +4,16 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
   update: update
 });
 
+var TILE_SIZE = 64;
+
 function preload() {
   console.log('preload');
+  game.load.spritesheet('runner', 'assets/graphics/runner.png', TILE_SIZE, TILE_SIZE * 2);
 }
 function create() {
   console.log('create');
+  game.add.sprite(0, 0, 'runner');
+  game.stage.backgroundColor = "#eeeeee";
 }
 function update() {
 }
