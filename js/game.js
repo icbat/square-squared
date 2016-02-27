308e1a30598a69badbc2c6c5cb1d343c77f99840
Evan Cobb <cobb.evan@gmail.com>
2016-02-27 16:55:14 -0600

    square jumps on touch

--- js/game.js
+++ js/game.js
@@ -1,7 +1,8 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
   preload: preload,
   create: create,
-  update: update
+  update: update,
+  render: render
 });
 
 var TILE_SIZE = 64;
@@ -15,8 +16,31 @@ function create() {
   game.stage.backgroundColor = "#666";
 
   runner = new Phaser.Rectangle(game.world.centerX - TILE_SIZE /2, game.world.centerY - TILE_SIZE/2, TILE_SIZE, TILE_SIZE);
-  game.debug.geom(runner,'#2ecc71');
+
+  game.input.onDown.add(onDown, this);
+  game.input.onUp.add(onUp, this);
 }
 
 function update() {
+}
+
+function render() {
+  game.debug.geom(runner,'#2ecc71');
+}
+
+function onDown(pointer, mouseEvent) {
+  if(mouseEvent.identifier === 0) {
+    console.log('down');
+    runner.y -= 100;
+    console.log(runner.y);
+  }
+}
+
+function onUp(pointer, mouseEvent) {
+  // Prevents 'mouse leaving the game world' from firing this, too
+  if(mouseEvent.identifier === 0 && pointer.identifier === 0) {
+    console.log('up', pointer);
+    runner.y += 100;
+    console.log(runner.y);
+  }
 }
