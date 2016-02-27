ea949d6e341b6cfc27ea2ff590f988ac175ada06
Evan Cobb <cobb.evan@gmail.com>
2016-02-27 16:20:22 -0600

    Setting the runner to the center of the screen

--- js/game.js
+++ js/game.js
@@ -5,6 +5,7 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
 });
 
 var TILE_SIZE = 64;
+var runner;
 
 function preload() {
   console.log('preload');
@@ -14,7 +15,8 @@ function create() {
   console.log('create');
   game.stage.backgroundColor = "#eeeeee";
 
-  var runner = game.add.sprite(0, 0, 'runner');
+  runner = game.add.sprite(game.world.centerX, game.world.centerY, 'runner');
+  runner.anchor.setTo(0.5, 0.5);
   var animation = runner.animations.add('run');
   runner.animations.play('run', 10, true);
 }
