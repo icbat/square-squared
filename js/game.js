b498745efde274a0d9a8ac1308d34502b5911a80
Evan Cobb <cobb.evan@gmail.com>
2016-02-27 17:04:52 -0600

    drawing a ground, moving the runner down to it

--- js/game.js
+++ js/game.js
@@ -6,7 +6,8 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
 });
 
 var TILE_SIZE = 64;
-var runner;
+var maxJumpHeight = 200;
+var runner, ground;
 var colors = {
   green: '#2ecc71',
   lightGrey: '#ddd',
@@ -21,7 +22,8 @@ function create() {
   console.log('create');
   game.stage.backgroundColor = colors.lightGrey;
 
-  runner = new Phaser.Rectangle(game.world.centerX - TILE_SIZE /2, game.world.centerY - TILE_SIZE/2, TILE_SIZE, TILE_SIZE);
+  runner = new Phaser.Rectangle(game.world.centerX - TILE_SIZE / 2, game.world.height - TILE_SIZE * 2, TILE_SIZE, TILE_SIZE);
+  ground = new Phaser.Rectangle(0, game.world.height - TILE_SIZE, game.world.width, TILE_SIZE);
 
   game.input.onDown.add(onDown, this);
   game.input.onUp.add(onUp, this);
@@ -32,12 +34,13 @@ function update() {
 
 function render() {
   game.debug.geom(runner, colors.green);
+  game.debug.geom(ground, colors.darkGrey);
 }
 
 function onDown(pointer, mouseEvent) {
   if(mouseEvent.identifier === 0) {
     console.log('down');
-    runner.y -= 100;
+    runner.y -= maxJumpHeight;
     console.log(runner.y);
   }
 }
@@ -46,7 +49,7 @@ function onUp(pointer, mouseEvent) {
   // Prevents 'mouse leaving the game world' from firing this, too
   if(mouseEvent.identifier === 0 && pointer.identifier === 0) {
     console.log('up', pointer);
-    runner.y += 100;
+    runner.y += maxJumpHeight;
     console.log(runner.y);
   }
 }
