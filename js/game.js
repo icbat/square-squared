f7df77dee06d39a37c2d7250fbf09a271be84f3e
Evan Cobb <cobb.evan@gmail.com>
2016-02-27 18:11:40 -0600

    making a static obstacle thing

--- js/game.js
+++ js/game.js
@@ -7,22 +7,25 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
 
 var TILE_SIZE = 64;
 var maxJumpHeight = 200;
-var runner, ground;
+var groundHeight;
+var runner, ground, obstacle;
 var colors = {
-  green: '#2ecc71',
+  green: '#22CC77',
   lightGrey: '#ddd',
   middleGrey: '#888',
   darkGrey: '#333'
 };
 
 function preload() {
+  groundHeight = game.world.height - TILE_SIZE;
 }
 
 function create() {
   game.stage.backgroundColor = colors.lightGrey;
 
-  runner = new Phaser.Rectangle(game.world.centerX - TILE_SIZE / 2, game.world.height - TILE_SIZE * 2, TILE_SIZE, TILE_SIZE);
-  ground = new Phaser.Rectangle(0, game.world.height - TILE_SIZE, game.world.width, TILE_SIZE);
+  runner = new Phaser.Rectangle(game.world.centerX - TILE_SIZE / 2, groundHeight - TILE_SIZE, TILE_SIZE, TILE_SIZE);
+  ground = new Phaser.Rectangle(0, groundHeight, game.world.width, TILE_SIZE);
+  obstacle = new Phaser.Rectangle(game.world.width, groundHeight - TILE_SIZE, TILE_SIZE, TILE_SIZE);
 
   game.input.onDown.add(onDown, this);
   game.input.onUp.add(onUp, this);
@@ -34,6 +37,8 @@ function update() {
 function render() {
   game.debug.geom(runner, colors.green);
   game.debug.geom(ground, colors.darkGrey);
+
+  game.debug.geom(obstacle, colors.middleGrey);
 }
 
 function onDown(pointer, mouseEvent) {
