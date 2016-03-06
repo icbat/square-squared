563bf2cc48be1b343f851c942c879048a995f150
Evan Cobb <cobb.evan@gmail.com>
2016-03-05 22:44:14 -0600

    pulling data off of game object

--- js/game.js
+++ js/game.js
@@ -1,11 +1,11 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO);
 
-game.constants = {
+constants = {
   tileSize: 64,
   maxJumpHeight: 200
 };
-game.objects = {};
-game.colorPalette = {
+objects = {};
+colorPalette = {
   accent: '#22CC77',
   light: '#ddd',
   middle: '#888',
@@ -18,43 +18,43 @@ states.running = {
   preload: function () {},
 
   create: function () {
-    var groundHeight = game.world.height - game.constants.tileSize;
-    game.stage.backgroundColor = game.colorPalette.light;
+    var groundHeight = game.world.height - constants.tileSize;
+    game.stage.backgroundColor = colorPalette.light;
 
-    game.objects.runner = new Phaser.Rectangle(game.world.centerX - game.constants.tileSize / 2, groundHeight - game.constants.tileSize, game.constants.tileSize, game.constants.tileSize);
-    game.objects.ground = new Phaser.Rectangle(0, groundHeight, game.world.width, game.constants.tileSize);
-    game.objects.obstacle = new Phaser.Rectangle(game.world.width + 20, groundHeight - game.constants.tileSize, game.constants.tileSize, game.constants.tileSize);
+    objects.runner = new Phaser.Rectangle(game.world.centerX - constants.tileSize / 2, groundHeight - constants.tileSize, constants.tileSize, constants.tileSize);
+    objects.ground = new Phaser.Rectangle(0, groundHeight, game.world.width, constants.tileSize);
+    objects.obstacle = new Phaser.Rectangle(game.world.width + 20, groundHeight - constants.tileSize, constants.tileSize, constants.tileSize);
 
     game.input.onDown.add(this.onDown, this);
     game.input.onUp.add(this.onUp, this);
   },
 
   update: function () {
-    game.objects.obstacle.x -= 2;
-    if (game.objects.obstacle.x < -20) {
-      game.objects.obstacle.x = game.world.width + 20;
+    objects.obstacle.x -= 2;
+    if (objects.obstacle.x < -20) {
+      objects.obstacle.x = game.world.width + 20;
     }
-    if(game.objects.obstacle.intersects(game.objects.runner)) {
+    if(objects.obstacle.intersects(objects.runner)) {
       game.state.start('waiting');
     }
   },
 
   render: function () {
-    game.debug.geom(game.objects.runner, game.colorPalette.accent);
-    game.debug.geom(game.objects.ground, game.colorPalette.dark);
-    game.debug.geom(game.objects.obstacle, game.colorPalette.middle);
+    game.debug.geom(objects.runner, colorPalette.accent);
+    game.debug.geom(objects.ground, colorPalette.dark);
+    game.debug.geom(objects.obstacle, colorPalette.middle);
   },
 
   onDown: function (pointer, mouseEvent) {
     if(mouseEvent.identifier === 0) {
-      game.objects.runner.y -= game.constants.maxJumpHeight;
+      objects.runner.y -= constants.maxJumpHeight;
     }
   },
 
   onUp: function (pointer, mouseEvent) {
     // Prevents 'mouse leaving the game world' from firing this, too
     if(mouseEvent.identifier === 0 && pointer.identifier === 0) {
-      game.objects.runner.y += game.constants.maxJumpHeight;
+      objects.runner.y += constants.maxJumpHeight;
     }
   }
 };
@@ -63,18 +63,18 @@ states.waiting = {
   preload: function () {},
 
   create: function () {
-    var groundHeight = game.world.height - game.constants.tileSize;
-    game.stage.backgroundColor = game.colorPalette.light;
+    var groundHeight = game.world.height - constants.tileSize;
+    game.stage.backgroundColor = colorPalette.light;
 
-    game.objects.runner = new Phaser.Rectangle(game.world.centerX - game.constants.tileSize / 2, groundHeight - game.constants.tileSize, game.constants.tileSize, game.constants.tileSize);
-    game.objects.ground = new Phaser.Rectangle(0, groundHeight, game.world.width, game.constants.tileSize);
+    objects.runner = new Phaser.Rectangle(game.world.centerX - constants.tileSize / 2, groundHeight - constants.tileSize, constants.tileSize, constants.tileSize);
+    objects.ground = new Phaser.Rectangle(0, groundHeight, game.world.width, constants.tileSize);
 
     game.input.onTap.add(this.startRunning, this);
   },
 
   render: function () {
-    game.debug.geom(game.objects.runner, game.colorPalette.accent);
-    game.debug.geom(game.objects.ground, game.colorPalette.dark);
+    game.debug.geom(objects.runner, colorPalette.accent);
+    game.debug.geom(objects.ground, colorPalette.dark);
   },
 
   startRunning: function() {
