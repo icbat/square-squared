4cea40b8e1207a11c987eeb6b818dd4ed7fc11d5
Evan Cobb <cobb.evan@gmail.com>
2016-03-05 22:59:05 -0600

    giving the states their own reference to the game object
    I think...

--- js/game.js
+++ js/game.js
@@ -14,71 +14,79 @@ colorPalette = {
 
 states = {};
 
-states.running = {
-  create: function () {
-    game.input.onDown.add(this.onDown, this);
-    game.input.onUp.add(this.onUp, this);
-  },
+states.running = function(game) {
+  return {
+    create: function () {
+      game.input.onDown.add(this.onDown, this);
+      game.input.onUp.add(this.onUp, this);
+    },
 
-  update: function () {
-    objects.obstacle.x -= 2;
-    if (objects.obstacle.x < -20) {
-      objects.obstacle.x = game.world.width + 20;
-    }
-    if(objects.obstacle.intersects(objects.runner)) {
-      game.state.start('waiting');
-    }
-  },
+    update: function () {
+      objects.obstacle.x -= 2;
+      if (objects.obstacle.x < -20) {
+        objects.obstacle.x = game.world.width + 20;
+      }
+      if(objects.obstacle.intersects(objects.runner)) {
+        game.state.start('waiting');
+      }
+    },
 
-  render: function () {
-    game.debug.geom(objects.runner, colorPalette.accent);
-    game.debug.geom(objects.ground, colorPalette.dark);
-    game.debug.geom(objects.obstacle, colorPalette.middle);
-  },
+    render: function () {
+      game.debug.geom(objects.runner, colorPalette.accent);
+      game.debug.geom(objects.ground, colorPalette.dark);
+      game.debug.geom(objects.obstacle, colorPalette.middle);
+    },
 
-  onDown: function (pointer, mouseEvent) {
-    if(mouseEvent.identifier === 0) {
-      objects.runner.y -= constants.maxJumpHeight;
-    }
-  },
+    onDown: function (pointer, mouseEvent) {
+      if(mouseEvent.identifier === 0) {
+        objects.runner.y -= constants.maxJumpHeight;
+      }
+    },
 
-  onUp: function (pointer, mouseEvent) {
-    // Prevents 'mouse leaving the game world' from firing this, too
-    if(mouseEvent.identifier === 0 && pointer.identifier === 0) {
-      objects.runner.y += constants.maxJumpHeight;
+    onUp: function (pointer, mouseEvent) {
+      // Prevents 'mouse leaving the game world' from firing this, too
+      if(mouseEvent.identifier === 0 && pointer.identifier === 0) {
+        objects.runner.y += constants.maxJumpHeight;
+      }
     }
-  }
+  };
 };
 
-states.waiting = {
-  create: function () {
-    objects.obstacle.setTo(game.world.width + 20, constants.groundHeight - constants.tileSize, constants.tileSize, constants.tileSize);
-    game.input.onTap.add(this.startRunning, this);
-  },
+states.waiting = function(game) {
+  return {
+    create: function () {
+      objects.obstacle.setTo(game.world.width + 20, constants.groundHeight - constants.tileSize, constants.tileSize, constants.tileSize);
+      game.input.onTap.add(this.startRunning, this);
+    },
 
-  render: function () {
-    game.debug.geom(objects.runner, colorPalette.accent);
-    game.debug.geom(objects.ground, colorPalette.dark);
-  },
+    render: function () {
+      game.debug.geom(objects.runner, colorPalette.accent);
+      game.debug.geom(objects.ground, colorPalette.dark);
+    },
 
-  startRunning: function() {
-    game.state.start('running');
-  }
+    startRunning: function() {
+      game.state.start('running');
+    }
+  };
 };
 
-states.init = {
-  preload: function() {
-    constants.groundHeight = game.world.height - constants.tileSize;
-    constants.centerX = game.world.centerX - constants.tileSize / 2;
-    game.stage.backgroundColor = colorPalette.light;
-    objects.runner = new Phaser.Rectangle(constants.centerX, constants.groundHeight - constants.tileSize, constants.tileSize, constants.tileSize);
-    objects.ground = new Phaser.Rectangle(0, constants.groundHeight, game.world.width, constants.tileSize);
-    objects.obstacle = new Phaser.Rectangle(game.world.width + 20, constants.groundHeight - constants.tileSize, constants.tileSize, constants.tileSize);
-  },
+states.init = function(game) {
+  return {
+    preload: function() {
+      game.stage.backgroundColor = colorPalette.light;
 
-  create: function() {
-    game.state.start('running');
-  }
+      constants.groundHeight = game.world.height - constants.tileSize;
+      constants.centerX = game.world.centerX - constants.tileSize / 2;
+      
+      objects.runner = new Phaser.Rectangle(constants.centerX, constants.groundHeight - constants.tileSize, constants.tileSize, constants.tileSize);
+      objects.ground = new Phaser.Rectangle(0, constants.groundHeight, game.world.width, constants.tileSize);
+      objects.obstacle = new Phaser.Rectangle(game.world.width + 20, constants.groundHeight - constants.tileSize, constants.tileSize, constants.tileSize);
+    },
+
+    create: function() {
+      game.state.start('running');
+    }
+  };
 };
 
 game.state.add('init', states.init);
