468a15d7b5079ba8bb9ef5f0297db06f8283dcec
Evan Cobb <cobb.evan@gmail.com>
2016-05-29 15:25:29 -0500

    Adding gulp tasks for building the HTML5 distributable

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 var gulp = require('gulp');
+var zip = require('gulp-zip');
+
+var build_directory = 'build';
 
 gulp.task('default', ['copy-bower'], function() {});
 
@@ -10,4 +13,36 @@ gulp.task('copy-bower', function() {
   return gulp
     .src(bower_components)
     .pipe(gulp.dest('./js/vendor/'));
+});
+
+gulp.task('build', ['stage-root', 'stage-assets', 'stage-js'], function() {
+  return gulp
+  .src(build_directory + "/**")
+  .pipe(zip('endless-running.zip'))
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('stage-root', function() {
+  return gulp
+  .src([
+    'index.html',
+    'LICENSE',
+    'README.md'
+  ])
+  .pipe(gulp.dest(build_directory))
+  ;
+});
+
+gulp.task('stage-assets', function() {
+  return gulp
+  .src('assets/**')
+  .pipe(gulp.dest(build_directory + '/assets'))
+  ;
+});
+
+gulp.task('stage-js', ['copy-bower'], function() {
+  return gulp
+  .src('js/**')
+  .pipe(gulp.dest(build_directory + '/js'))
+  ;
 });
