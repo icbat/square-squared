ea87e8de3389125581c1099fc93592e233d2d0a9
Evan Cobb <cobb.evan@gmail.com>
2016-05-30 15:10:30 -0500

    Actually centering text instead of approximately centering

--- js/states/waiting.js
+++ js/states/waiting.js
@@ -4,7 +4,8 @@ state_waiting = function(game) {
       objects.obstacle.setTo(game.world.width + 20, constants.groundHeight - constants.tileSize, constants.tileSize, constants.tileSize);
       game.input.onTap.add(this.startRunning, this);
       var textStyle = {fill: colorPalette.accent, boundsAlignH: "center", boundsAlignV: "middle"};
-      var text = game.add.text(game.world.centerX - 150, game.world.centerY, "Touch anywhere to jump", textStyle);
+      var text = game.add.text(game.world.centerX, game.world.centerY, "Touch anywhere to jump", textStyle);
+      text.anchor.set(0.5);
       text.setShadow(2, 2, colorPalette.dark);
     },
 
