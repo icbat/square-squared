95d04ffa7c92fccae55296af1944d170061645ff
Evan Cobb <cobb.evan@gmail.com>
2016-06-08 01:46:31 -0500

    Adding down-up text

--- www/js/debugText.js
+++ www/js/debugText.js
@@ -1,12 +1,18 @@
+var debugListeners = [];
 var debugText = [];
 
 var enableDebugging = function(debugIsVisible) {
     // Remove potentially stale references
     var i;
+    for (i = 0; i < debugListeners.length; ++i) {
+      var listener = debugListeners[i];
+      listener.detach();
+    }
+    debugListeners = [];
     var text;
     for (i = 0; i < debugText.length; ++i) {
-      text = debugText[i];
-      text.destroy();
+        text = debugText[i];
+        text.destroy();
     }
     debugText = [];
 
@@ -18,6 +24,17 @@ var enableDebugging = function(debugIsVisible) {
         boundsAlignV: "top"
     };
 
+    debugText.push(game.add.text(0, 16, "touch down " + false, debugStyle));
+    game.input.onDown.add(function() {
+      if(debugText.length > 0){
+        debugText[0].text = "touch down " + true;
+      }
+    });
+    game.input.onUp.add(function() {
+      if(debugText.length > 0){
+        debugText[0].text = "touch down " + false;
+      }
+    });
     debugText.push(game.add.text(0, 0, "world " + game.world.height + "h x " + game.world.width + "w", debugStyle));
 
     // Set visibility
