7afd1c568cffe53e2a3a00673deeed91fa2ca8a3
Evan Cobb <cobb.evan@gmail.com>
2016-06-09 20:52:11 -0500

    Simplifying row debugging text height.

--- www/js/debugText.js
+++ www/js/debugText.js
@@ -8,18 +8,18 @@ var lastJump = {
 
 var drawDebugText = function(shouldDraw) {
     if (shouldDraw) {
-        var y = 0;
-        var debugRowConstant = 16;
-        game.debug.text("world " + game.world.height + "h x " + game.world.width + "w", 0, y += debugRowConstant, colorPalette.debugColor);
-        game.debug.text("player height: " + (constants.groundHeight - objects.runner.findLowestPoint()), 0, y += debugRowConstant, colorPalette.debugColor);
-        game.debug.text("player vspeed: " + objects.runner.vspeed, 0, y += debugRowConstant, colorPalette.debugColor);
-        y += debugRowConstant;
-        game.debug.text("Last Jump ", 0, y += debugRowConstant, colorPalette.debugColor);
-        game.debug.text("Drag on Y " + lastJump.dragY, 0, y += debugRowConstant, colorPalette.debugColor);
-        game.debug.text("% of screen dragged " + lastJump.percentOfScreenDragged, 0, y += debugRowConstant, colorPalette.debugColor);
-        game.debug.text("Charge Level " + lastJump.chargeLevel, 0, y += debugRowConstant, colorPalette.debugColor);
-        game.debug.text("Charge Coefficient " + lastJump.chargeCoefficient, 0, y += debugRowConstant, colorPalette.debugColor);
-        game.debug.text("JumpStrength " + lastJump.jumpStrength, 0, y += debugRowConstant, colorPalette.debugColor);
+        var row = 1;
+        var rowHeight = 16;
+        game.debug.text("world " + game.world.height + "h x " + game.world.width + "w", 0, row++ * rowHeight, colorPalette.debugColor);
+        game.debug.text("player height: " + (constants.groundHeight - objects.runner.findLowestPoint()), 0, row++ * rowHeight, colorPalette.debugColor);
+        game.debug.text("player vspeed: " + objects.runner.vspeed, 0, row++ * rowHeight, colorPalette.debugColor);
+        row++;
+        game.debug.text("Last Jump ", 0, row++ * rowHeight, colorPalette.debugColor);
+        game.debug.text("Drag on Y " + lastJump.dragY, 0, row++ * rowHeight, colorPalette.debugColor);
+        game.debug.text("% of screen dragged " + lastJump.percentOfScreenDragged, 0, row++ * rowHeight, colorPalette.debugColor);
+        game.debug.text("Charge Level " + lastJump.chargeLevel, 0, row++ * rowHeight, colorPalette.debugColor);
+        game.debug.text("Charge Coefficient " + lastJump.chargeCoefficient, 0, row++ * rowHeight, colorPalette.debugColor);
+        game.debug.text("JumpStrength " + lastJump.jumpStrength, 0, row++ * rowHeight, colorPalette.debugColor);
         game.debug.pointer(game.input.activePointer, false, 'rgba(0,255,0,0.5)', 'rgba(255,0,0,0.5)', colorPalette.debugColor);
     }
 };
