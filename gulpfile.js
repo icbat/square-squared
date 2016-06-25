499b4bd7198ca26c153d481aa6b5b562b3dfd9c7
Evan Cobb <cobb.evan@gmail.com>
2016-06-25 13:26:04 -0500

    tech - removing unused gulp stuff, adding one that copies assets to the right place

--- gulpfile.js
+++ gulpfile.js
@@ -3,47 +3,27 @@ var zip = require('gulp-zip');
 
 var build_directory = 'build';
 
-gulp.task('default', ['copy-bower'], function() {});
+gulp.task('default', ['copy-bower', 'copy-graphics', 'copy-sound'], function() {});
 
 var bower_components = [
-  'bower_components/phaser/build/phaser.min.js',
-  'bower_components/phaser-state-transition-plugin/dist/phaser-state-transition-plugin.min.js'
+    'bower_components/phaser/build/phaser.min.js',
+    'bower_components/phaser-state-transition-plugin/dist/phaser-state-transition-plugin.min.js'
 ];
 
 gulp.task('copy-bower', function() {
-  return gulp
-    .src(bower_components)
-    .pipe(gulp.dest('./www/js/vendor/'));
+    return gulp
+        .src(bower_components)
+        .pipe(gulp.dest('./www/js/vendor/'));
 });
 
-gulp.task('build', ['stage-root', 'stage-assets', 'stage-js'], function() {
-  return gulp
-  .src(build_directory + "/**")
-  .pipe(zip('endless-running.zip'))
-  .pipe(gulp.dest('dist'));
+gulp.task('copy-sound', function() {
+    return gulp
+        .src('assets/sound/*')
+        .pipe(gulp.dest('./www/assets/sounds'));
 });
 
-gulp.task('stage-root', function() {
-  return gulp
-  .src([
-    'index.html',
-    'LICENSE',
-    'README.md'
-  ])
-  .pipe(gulp.dest(build_directory))
-  ;
-});
-
-gulp.task('stage-assets', function() {
-  return gulp
-  .src('assets/**')
-  .pipe(gulp.dest(build_directory + '/assets'))
-  ;
-});
-
-gulp.task('stage-js', ['copy-bower'], function() {
-  return gulp
-  .src('js/**')
-  .pipe(gulp.dest(build_directory + '/js'))
-  ;
+gulp.task('copy-graphics', function() {
+    return gulp
+        .src('assets/browser/favicon.png')
+        .pipe(gulp.dest('./www/assets/graphics'));
 });
