c76e5704c44763514429d6b6e7674157178c1538
Evan Cobb <cobb.evan@gmail.com>
2016-06-26 13:27:01 -0500

    tech - combining dying state in to running state

--- www/js/phaser-start.js
+++ www/js/phaser-start.js
@@ -1,5 +1,4 @@
 game.state.add('init', state_init);
 game.state.add('running', state_running);
 game.state.add('waiting', state_waiting);
-game.state.add('dying', state_dying);
 game.state.start('init');
--- www/js/states/running.js
+++ www/js/states/running.js
@@ -40,43 +40,56 @@ var state_running = function(game) {
         },
 
         update: function(game) {
-            objects.runner.applyGravity();
-
-            var obstacleIndex;
-            for (obstacleIndex = 0; obstacleIndex < objects.obstacles.length; ++obstacleIndex) {
-                var obstacle = objects.obstacles[obstacleIndex];
-                obstacle.applyGravity();
-                obstacle.movePolygonBy(constants.hspeed);
-
-                if (objects.runner.intersects(obstacle)) {
-                    // Game over
-                    game.stateTransition.to('dying', false);
-                    return;
+            if (this.gameIsLost) {
+                if (this.colorStep <= 50) {
+                    objects.runner.color = Phaser.Color.interpolateColor(this.runnerOriginalColor, colorPalette.background, 50, this.colorStep++);
                 }
-
-                if (runnerHasPassedObstacle(obstacle, objects.runner)) {
-                    this.scorePoint(obstacle);
+                if (game.time.time > this.timeToLeave) {
+                    var highScore = localStorage.getItem('squareSquared-highScore');
+                    localStorage.setItem('squareSquared-highScore', Math.max(gameState.score, highScore ? highScore : 0));
+                    objects.runner.color = colorPalette.runner;
+                    game.stateTransition.to('waiting');
+                    this.gameIsLost = false;
                 }
+            } else {
+                objects.runner.applyGravity();
 
-            }
-            if (objects.obstacles[0].findRightmostPoint() < 0) {
-                var removed = objects.obstacles.shift();
-                objects.runner.onLand.remove(removed.runnerLandCallback);
-            }
-            if (objects.obstacles[objects.obstacles.length - 1].findRightmostPoint() < game.world.width) {
-                obstacleGenerator.addObstacleToBack();
-            }
-            this.dragY = this.firstTouchY - game.input.activePointer.worldY;
-            var touchIsDown = this.firstTouchY !== null;
-            var charge = chargeLevel(this.dragY, game.world.height, touchIsDown);
-            objects.runner.updateBeforeDraw(charge, touchIsDown);
-            objects.leftJumpLine.moveToX(objects.leftJumpLine.xPos);
-            objects.rightJumpLine.moveToX(objects.rightJumpLine.xPos);
+                var obstacleIndex;
+                for (obstacleIndex = 0; obstacleIndex < objects.obstacles.length; ++obstacleIndex) {
+                    var obstacle = objects.obstacles[obstacleIndex];
+                    obstacle.applyGravity();
+                    obstacle.movePolygonBy(constants.hspeed);
 
-            if (this.dragY > 0) {
-                objects.dragLine.setHeight(this.dragY);
+                    if (objects.runner.intersects(obstacle)) {
+                        // Game over
+                        loseGame(this);
+                        return;
+                    }
+
+                    if (runnerHasPassedObstacle(obstacle, objects.runner)) {
+                        this.scorePoint(obstacle);
+                    }
+
+                }
+                if (objects.obstacles[0].findRightmostPoint() < 0) {
+                    var removed = objects.obstacles.shift();
+                    objects.runner.onLand.remove(removed.runnerLandCallback);
+                }
+                if (objects.obstacles[objects.obstacles.length - 1].findRightmostPoint() < game.world.width) {
+                    obstacleGenerator.addObstacleToBack();
+                }
+                this.dragY = this.firstTouchY - game.input.activePointer.worldY;
+                var touchIsDown = this.firstTouchY !== null;
+                var charge = chargeLevel(this.dragY, game.world.height, touchIsDown);
+                objects.runner.updateBeforeDraw(charge, touchIsDown);
+                objects.leftJumpLine.moveToX(objects.leftJumpLine.xPos);
+                objects.rightJumpLine.moveToX(objects.rightJumpLine.xPos);
+
+                if (this.dragY > 0) {
+                    objects.dragLine.setHeight(this.dragY);
+                }
+                objects.dragLine.color = constants.chargeColors[charge];
             }
-            objects.dragLine.color = constants.chargeColors[charge];
         },
 
         scorePoint: function(obstacle) {
@@ -92,12 +105,12 @@ var state_running = function(game) {
             this.graphics.clear();
             objects.leftJumpLine.draw(this.graphics);
             objects.rightJumpLine.draw(this.graphics);
+            objects.runner.draw(this.graphics);
             var obstacleIndex;
             for (obstacleIndex = 0; obstacleIndex < objects.obstacles.length; ++obstacleIndex) {
                 var obstacle = objects.obstacles[obstacleIndex];
                 obstacle.draw(this.graphics);
             }
-            objects.runner.draw(this.graphics);
             objects.ground.draw(this.graphics);
             drawDebugText(constants.debugMode);
             objects.dragLine.draw(this.graphics);
@@ -129,4 +142,12 @@ var state_running = function(game) {
 
 var runnerHasPassedObstacle = function(obstacle, runner) {
     return !obstacle.hasScored && Math.round(obstacle.findRightmostPoint()) < runner.findLeftmostPoint() - 1;
+};
+var loseGame = function(context) {
+    context.gameIsLost = true;
+    context.timeToLeave = game.time.time + constants.timeOnDyingScreen;
+    context.colorStep = 0;
+    context.runnerOriginalColor = objects.runner.color;
+    var loseSound = game.sound.play('lose', 0.1);
+    loseSound._sound.playbackRate.value = 0.5;
 };
