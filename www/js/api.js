51d8c8cf7f85f5bddb2e38f6d0c730b901e9c9bd
Evan Cobb <cobb.evan@gmail.com>
2016-07-18 19:25:30 -0500

    analytics - adding a UUID per device/install to correlate events to a "user"

--- www/js/api.js
+++ www/js/api.js
@@ -1,11 +1,21 @@
+var getUUID = function() {
+    var uuid = localStorage.getItem('squareSquared-uuid');
+    if (!uuid) {
+        uuid = new Phaser.RandomDataGenerator([Date.now()]).uuid();
+        localStorage.setItem('squareSquared-uuid', uuid);
+    }
+    return uuid;
+};
+
 var api = {
-    url: "https://square-squared-stats.herokuapp.com/"
+    url: "https://square-squared-stats.herokuapp.com/",
+    uuid: getUUID()
 };
 
 api.report = {
     launch: function() {
         var payload = JSON.stringify({
-            "replaceMe": "withUUID"
+            "uuid": api.uuid
         });
         $.post(api.url + "launch", payload, null, "json");
     }
