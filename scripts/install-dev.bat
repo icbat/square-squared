f45727058cf993d39e7ab9a1a4a0b489f6ac0d07
Evan Cobb <cobb.evan@gmail.com>
2016-07-26 14:52:38 -0500

    adding dev install and run scripts to help me use multiple boxen

---
+++ scripts/install-dev.bat
@@ -0,0 +1,7 @@
+npm install
+bower install
+cordova platform add android
+cordova plugin add cordova-plugin-whitelist
+cordova plugin add cordova-plugin-splashscreen
+xcopy "C:\Users\cobbe\Google Drive\squareSquared\assets" .\assets\ /e
+gulp
